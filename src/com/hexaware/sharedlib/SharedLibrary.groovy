4654d9a6e8cfd433fca133638c2caf309ff8880c
snehashingate <snehashingate58@gmail.com>
2021-12-20 11:34:16 +0530

    Create SharedLibrary.groovy

---
+++ src/com/hexaware/sharedlib/SharedLibrary.groovy
@@ -0,0 +1,14 @@
+package com.hexaware.sharedlib;
+
+public class SharedLibrary {
+  def pipeline
+  
+  public SharedLibrary(pipeline) {
+    pipeline.steps.echo(pipeline.getClass().toString())
+    this.pipeline = pipeline
+  }
+  
+  public void startBuild() {
+    pipeline.steps.sh "echo 'Hello, World!'"
+  }
+}
